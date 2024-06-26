b70ca12d302e4781f7c58c9036e0c97fc97e46cd
alexander.boyarovich <alexander.boyarovich@practicalse.com>
2024-06-26 15:29:11 +0300

    setup config and logger

---
+++ internal/config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env      string `yaml:"env" env-default:"local"`
+	GRPC     `yaml:"grpc_server"`
+	Storage  `yaml:"storage"`
+	TokenTTL time.Duration `yaml:"token_ttl" env-default:"1h"`
+}
+
+type GRPC struct {
+	Host         string        `yaml:"host" env-default:"localhost"`
+	Port         string        `yaml:"port" env-default:"8080"`
+	ReadTimeout  time.Duration `yaml:"read_timeout" env-default:"4s"`
+	WriteTimeout time.Duration `yaml:"write_timeout" env-default:"4s"`
+}
+
+type Storage struct {
+	DbName   string `yaml:"db_name" env-required:"true"`
+	User     string `yaml:"user" env-required:"true"`
+	Password string `yaml:"password" env-required:"true"`
+	Host     string `yaml:"host" env-required:"true"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	// check config if the file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file not found: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+	return &cfg
+}
